35f2b4d2180e9e167674d4144f5c3afdcbb9b339
Manjunatha Shety H <manjunatha.shetty_h@nokia.com>
2017-09-07 12:39:11 +0530

    Added unit test case

---
+++ factory_test.go
@@ -0,0 +1,46 @@
+package threadpool
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+const (
+	NumberOfWorkes = 20
+	QueueSize = int64(1000)
+)
+
+var (
+	Pool *ThreadPool
+)
+
+func TestNewThreadPool(t *testing.T) {
+	Pool = NewThreadPool(NumberOfWorkes,QueueSize)
+}
+
+func TestThreadPool_Execute(t *testing.T) {
+	data:= &Data{Val:"pristine"}
+	task:=&TestTask{TestData:data}
+	Pool.Execute(task)
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("")
+
+	if data.Val != "changed" {
+		t.Fail()
+	}
+}
+
+type TestTask struct {
+	TestData *Data
+}
+
+type Data struct {
+	Val string
+}
+
+func (t *TestTask) Run(){
+	fmt.Println("Running the task")
+	t.TestData.Val="changed"
+}
