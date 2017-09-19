c1bcd73ce5aef540a85e59ad7951be87a06d22e4
manjunatha.shetty_h@nokia.com <manjunatha.shetty_h@nokia.com>
2017-09-19 11:50:54 +0530

    Added callable initial structure

---
+++ callable.go
@@ -0,0 +1,18 @@
+package threadpool
+
+type Callable interface {
+	Call() interface{}
+}
+
+type CallableTask struct {
+	Task Callable
+	Handle chan Future
+}
+
+type Future struct {
+	response interface{}
+}
+
+func (f *Future) Get() interface{}{
+	return f.response
+}
